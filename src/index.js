670bbcd81015c12fe11a79b6bf87d9f05ffcf332
runway88 <te2tttt@gmail.com>
2018-11-26 17:11:35 +0900

    add compo2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import {pg1,SampleList,Hello} from './pg1';
 ReactDOM.render(
   <App />,
   document.getElementById('d3')
@@ -23,34 +23,7 @@ ReactDOM.render(
   document.getElementById('list')
 );
 
-class Hello extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { msg: 'Hello!' };
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>{this.state.msg}</h1>
-        <button onClick={() => this.setState({msg: 'Bye!'})}>Click</button>
-      </div>
-    );
-  }
-}
-
-class SampleList extends React.Component {
-  render() {
-    const airports = [
-      { name: "MATSUSHIMA", legion: "TOUHOKU" },
-      { name: "KOMATSU", legion: "HOKURIKU" },
-      { name: "HYAKURI", legion: "KANTO" }
-    ];
-    const airportList = airports.map((airport, index) =>
-      <li key={index}>{airport.name} (Legion: {airport.legion})</li>
-    );
-    return (
-      <ul>{airports}</ul>
-    );
-  }
-}
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('hello')
+);
--- src/pg1.js
+++ src/pg1.js
@@ -10,4 +10,37 @@ import './App.css';
     );
   }
 }
+
+class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { msg: 'Hello!' };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.msg}</h1>
+        <button onClick={() => this.setState({msg: 'Bye!'})}>Click</button>
+      </div>
+    );
+  }
+}
+
+class SampleList extends React.Component {
+  render() {
+    const airports = [
+      { name: "MATSUSHIMA", legion: "TOUHOKU" },
+      { name: "KOMATSU", legion: "HOKURIKU" },
+      { name: "HYAKURI", legion: "KANTO" }
+    ];
+    const airportList = airports.map((airport, index) =>
+      <li key={index}>{airport.name} (Legion: {airport.legion})</li>
+    );
+    return (
+      <ul>{airports}</ul>
+    );
+  }
+}
+
  export default pg1;
