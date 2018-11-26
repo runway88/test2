9663b1a17465fe112be6a189644cb634da658f62
runway88 <te2tttt@gmail.com>
2018-11-26 16:35:31 +0900

    index.js err corrected8

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  <h1>Hello world!</h1>,
+  <h1>root 16:34 2018/11/26</h1>,
   document.getElementById('root')
+);
+
+ReactDOM.render(
+  <h1>test Hello world!</h1>,
+  document.getElementById('test')
 );
