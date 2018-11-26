eb6adaf91d106544280a3999ec00db5e8b433ea1
runway88 <te2tttt@gmail.com>
2018-11-26 16:56:28 +0900

    index.js err corrected

--- src/index.js
+++ src/index.js
@@ -23,6 +23,22 @@ ReactDOM.render(
   document.getElementById('list')
 );
 
+class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { msg: 'Hello!' };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.msg}</h1>
+        <button onClick={() => this.setState({msg: 'Bye!'})}>Click</button>
+      </div>
+    );
+  }
+}
+
 class SampleList extends React.Component {
   render() {
     const airports = [
@@ -30,7 +46,7 @@ class SampleList extends React.Component {
       { name: "KOMATSU", legion: "HOKURIKU" },
       { name: "HYAKURI", legion: "KANTO" }
     ];
-    const airportList = airport.map((airport, index) =>
+    const airportList = airports.map((airport, index) =>
       <li key={index}>{airport.name} (Legion: {airport.legion})</li>
     );
     return (
