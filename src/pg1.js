6fe06239bc6846f3976d22fada0055553e285193
runway88 <te2tttt@gmail.com>
2018-11-28 11:39:47 +0900

    構文修正

--- src/pg1.js
+++ src/pg1.js
@@ -61,9 +61,7 @@ export class CommentBox extends Component{
     return (
       <form onSubmit={this.submitHandler}>
       <input type="text" value={this.state.testValue} onChange = {this.handleChange} />
-      <button type="submit">
-        send
-      </button>
+      <button type="submit">send</button>
     );
   }
 };
